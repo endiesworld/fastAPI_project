e93aaeb492ea07b8c1348e32d6624b69fb3ffa3c
endiesworld <endiesworld@gmail.com>
2022-06-26 15:29:27 +0100

    Added pydantic for data validation

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
+from turtle import title
 from fastapi import Body, FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+
+class Post(BaseModel):
+    title: str
+    content: str
 
 
 @app.get("/")
@@ -13,7 +20,12 @@ async def get_post():
     return {'post': 'This is post message'}
 
 
+# @app.post("/createposts")
+# async def createposts(payload: dict = Body(...)):
+#     print(payload)
+#     return {'new_post': payload}
+
 @app.post("/createposts")
-async def createposts(payload: dict = Body(...)):
-    print(payload)
-    return {'message': 'Post created successfuly'}
+async def createposts(new_post: Post):
+    print(new_post)
+    return {'new_post': new_post}
